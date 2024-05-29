2b0409f9f876849e4d65391682fbe3ed07d5d579
Arayamariano <arayamariano67@gmail.com>
2024-05-29 11:34:05 -0300

    Correccion de errores

--- JS.js
+++ JS.js
@@ -1,11 +1,11 @@
 /*
-
+//Calcula el precio del palo dependiendo de la cantidad de carbono ingresada por el usuario
 
  */
 
 function calcularTotal() {
     const cantidad = document.getElementById('cantidad').value;
-    let precio = 3000;
+    let precio = 5000;
     const total = cantidad * precio;
-    document.getElementById('total').textContent = `Total: $${total.toFixed(2)}`;
+    document.getElementById('total').textContent = `Price: $${total.toFixed(2)}`;
 }
